ab7e042631a9a65daaeeb8e91349508b0fe575e8
walkingkode <duedumaxwell63@gmail.comm>
2023-12-14 03:04:48 -0800

    ✨task 1: modified variable check

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -4,7 +4,7 @@ number = random.randint(-10000, 10000)
 last_digit = number % 5
 if last_digit > 5:
     print(f"Last digit of {number} is {last_digit} and is greater than 5")
-elif number == 0:
+elif last_digit == 0:
     print(f"Last digit of {number} is {last_digit} and is 0")
-elif number < 6 and number != 0:
+elif last_digit < 6 and last_digit != 0:
     print(f"Last digit of {number} is {last_digit} and is less than 6 and not 0")
