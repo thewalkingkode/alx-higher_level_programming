7c0e73af29c38b211ffc90dd422700919b8d9da2
walkingkode <duedumaxwell63@gmail.comm>
2023-12-19 15:49:27 -0800

    task 3

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -1,12 +1,13 @@
 #!/usr/bin/python3
 import sys
+
 if __name__ == "__main__":
     num_args = len(sys.argv)
     arguments = sys.argv[1:]
 
-    if num_args == 0:
+    if len(arguments) == 0:
         print("0")
-    elif num_args > 1:
+    else:
         numbers = []
         for arg in arguments:
             number = int(arg)
