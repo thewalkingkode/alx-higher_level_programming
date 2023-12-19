bfe0335dab314b9c15780b5bdec209bbe1b17ba5
walkingkode <duedumaxwell63@gmail.comm>
2023-12-19 15:02:03 -0800

    task 3

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -3,6 +3,9 @@ import sys
 if __name__ == "__main__":
     num_args = len(sys.argv) - 1
     arguments = sys.argv[1:]
-    print("{} arguments:".format(num_args))
-    for index, arg in enumerate(arguments):
-        print("{}: {}".format(index + 1, arg))
+    if num_args == 0:
+        print("0 arguments.")
+    else:
+        print("{} argument:".format(num_args, 's' if num_args > 1 else ''))
+        for index, arg in enumerate(arguments):
+            print("{}: {}".format(index + 1, arg))
