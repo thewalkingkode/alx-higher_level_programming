551073b01f348be569afdc1896082b399758fac4
walkingkode <duedumaxwell63@gmail.comm>
2023-12-19 16:10:48 -0800

    task4

---
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+import hidden_4
+if __name__ == "__main__":
+
+    names = dir(hidden_4)
+    for name in names:
+        if name.startswith('__'):
+            continue
+        else:
+            print(name)
