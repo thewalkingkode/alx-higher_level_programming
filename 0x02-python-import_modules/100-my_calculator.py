3ed78d175439da819492511c6ade95ac5a39ad27
walkingkode <duedumaxwell63@gmail.comm>
2023-12-20 04:33:59 -0800

    pycodestyle fix

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -27,4 +27,3 @@ if __name__ == "__main__":
         print("Unknown operator. Available operators: +, -, * and /")
         sys.exit(1)
     print("{} {} {} = {}".format(a, operator, b, result))
-        
